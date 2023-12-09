2b618c3a6531ebc84dbd37b37d1e21aaccfa9882
Bruno F <39064476+bflud@users.noreply.github.com>
2023-12-09 18:16:28 -0300

    [Bruno F] Menu dinamico

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import { NavLink, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
-    const state = useSelector(state => state.handleCart)
-    const location = useLocation();
+    const [categories, setCategories] = useState([]);
+    const state = useSelector(state => state.handleCart);
 
+    const handleClick = (categoryID) => {
+        window.location.href = `/product?category=${categoryID}`;
+    };
 
-    const handleClick = event => {
-       const url =event.currentTarget.dataset.to;
-        window.location.href=url;
+    async function fetchCategories() {
+        try {
+            const response = await fetch('/mock-api/V1/categories/list');
+            if (!response.ok) {
+                throw new Error('Erro ao obter os dados da API');
+            }
+            const data = await response.json();
+            setCategories(data.items); 
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-      };
+    useEffect(() => {
+        fetchCategories(); 
+    }, []);
 
-    
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 sticky-top">
             <div className="container">
@@ -23,24 +36,21 @@ const Navbar = () => {
                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav m-auto my-2 text-center">
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/">PÁGINA INICIAL </NavLink>
+                            <NavLink className="nav-link" to="/">Página Inicial </NavLink>
                         </li>
+                        {/* Renderiza dinamicamente os itens da lista de categorias */}
+                        {categories.map(category => (
+                            <li className="nav-item" key={category.id}>
+                                <NavLink className="nav-link" to={`/product?category=${category.id}`} onClick={() => handleClick(category.id)}>
+                                    {category.name}
+                                </NavLink>
+                            </li>
+                        ))}
                         <li className="nav-item">
-                            <NavLink className="nav-link" data-to="/product?category=1" onClick={handleClick}>CAMISETAS</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" data-to="/product?category=2" onClick={handleClick}>CALÇAS</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" data-to="/product?category=3" onClick={handleClick}>SAPATOS</NavLink>
-                        </li>
-
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/contact">CONTATO</NavLink>
+                            <NavLink className="nav-link" to="/contact">Contato</NavLink>
                         </li>
                     </ul>
                     <div className="buttons text-center">
@@ -49,14 +59,10 @@ const Navbar = () => {
                             <button className="btn btn-danger" type="submit">Buscar</button>
                         </form>
                     </div>
-
-
                 </div>
-
-
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
